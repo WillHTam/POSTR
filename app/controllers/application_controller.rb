c3704be2d73f4f358f3b7771081c455aa913e783
William T <william.tam18@gmail.com>
2016-06-30 21:28:25 +0800

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,27 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user
+  helper_method :current_post
+  helper_method :user_logged_in?
+
+  def current_user
+    @current_user  ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_post
+    @current_post ||= Post.find[:post_id]
+  end
+
+  def user_logged_in?
+    current_user.present?
+  end
+
+  def authenticate_user
+    unless user_logged_in?
+      flash[:error] = "You must log in first."
+      redirect_to signin_path
+    end
+  end
+
+end
